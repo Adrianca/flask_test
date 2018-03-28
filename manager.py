5b4c326addd8e87ab7ff981c32e2367cb7acfcaa
Adrianca <ilybowiawiawy@163.com>
2018-03-28 11:03:17 +0800

    add manager.py

--- manager.py
+++ manager.py
@@ -1,1 +1,13 @@
 # -*- coding:utf-8 -*-
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello flask!'
+
+if __name__ == '__main__':
+    #运行 --> 相当于Django runserver. 都是框架自带的简易服务器
+    app.run()
